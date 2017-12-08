59df889c96daf678f3861ab961d33c3241b84ac3
Leah Holden <leah.holden@tufts.edu>
2017-12-08 14:22:41 -0500

    More polishing completed

--- README.md
+++ README.md
@@ -1,5 +1,7 @@
 # Backdoor Hunting Demo
-A quick program to help you think about how to detect backdoors on your own system
+A quick program to help developers and security professionals think about how to detect backdoors on their own systems. This is also an example of how easy it is to program your own monitoring functionality. I have previously written only one short program in Python prior to this program. Coding this project took me approximately 6 hours start to finish, including lots of polishing, commenting, planning, and writing out this README.
+
+I hope this will inspire others to think about what systems they have in place to look for suspicious activity on their own computers and networks.
 
 ## Modes
 
@@ -7,12 +9,16 @@ There are several ways that you can use the backdoor hunting demo: user-mediated
 
 #### Learning
 
-Learning mode is a precursor to autonomous monitoring. It runs for 12 hours, scanning once an hour, in order to get a read on what ports should normally be open. The program then writes what ports are 'normal' to a file that can then be used by autonomous mode.
+Learning mode is a precursor to autonomous monitoring. It runs for 1 hour, scanning every 10 seconds, in order to get a read on what ports should normally be open. The program then writes what ports are 'normal' to a file that can then be used by autonomous mode. One key part of this mode is that you should be fairly confident that all port activity is normal when you engage learning mode since this is the standard that we will rely on during autonomous monitoring mode.
+
+Learning mode can be finished at any time by pressing CTRL-C. The list of normal ports collected so far will still be written to a file.
 
 #### Autonomous Monitoring
 
 This mode requires that the user has run 'Learning' mode prior to use. This option will then use the file of 'normal' ports produced by learning mode in order to determine what ports are valid or not. 
 
+Autonomous monitoring mode can be exited at any time by pressing CTRL-C. 
+
 #### User-mediated Monitoring
 
-The simplest option, in this mode the program will scan your computer in order to see what ports are open. It will then go through the list of all open ports and ask you if you intended those ports to be open. At the end, it will print out the ports that the user thinks should be closed.
+This is the simplest option. In this mode, the program will scan your computer in order to see what ports are open. It will then go through the list of all open ports and ask you if you intended for those ports to be open. At the end, it will print out the ports that should be closed.
--- backdoorhunt.py
+++ backdoorhunt.py
@@ -24,10 +24,11 @@ parser.add_argument('-auto', metavar='PORTSFILE', help='Activates autonomous mon
 subprocess.call('clear', shell=True)
 
 # Subfunction for scanning your own computer for open ports
-def self_scan():
+def self_scan(quiet): # Quiet is a boolean which indicates if this function is in quiet mode
     i = 1
     list = []
-    print 'Progress |',
+    if not quiet: # Only prints out the progress bar if NOT in quiet mode, i.e. quiet is False
+        print 'Progress |',
     # Loop through and scan each potentially open port
     while i <= NUM_PORTS:
         # Sockets are used to connect to another computer
@@ -38,16 +39,18 @@ def self_scan():
             list.append(i)
         i = i + 1 # Increment port number
         if (i % PROGRESS_BAR) == 0: # Increases the level on the loading bar after 10% more of the scans are complete
-            sys.stdout.write('x')
-            sys.stdout.flush()
+            if not quiet:
+                sys.stdout.write('x')
+                sys.stdout.flush()
     # Source of code:
     # https://stackoverflow.com/questions/19196105/python-how-to-check-if-a-network-port-is-open-on-linux
-    print '|' # Ends progress bar
+    if not quiet:
+        print '|' # Ends progress bar
     return list # Returns the list of open ports
 
 # USER-MEDIATED MONITORING
 def user_monitoring():
-    ports = self_scan() # Scan yourself for open ports
+    ports = self_scan(False) # Scan yourself for open ports, False means verbose mode
     if not ports: #if empty list
         print "No ports open!"
     else:
@@ -73,21 +76,24 @@ def learning():
     i = 0
     scan = 1
     normal_ports = []
-    while i <= RUNNING_TIME:
-        # Once an hour, record the ports in use
-        print 'Starting scan #' + str(scan) + ': ' + str(i) + ' seconds in with ' + str(RUNNING_TIME - i) + ' seconds remaining'
-        ports = self_scan()
-        print 'Open ports: '
-        if ports is not []:
-            print ports
-        else:
-            print 'None'
-        for port in ports:
-            if port not in normal_ports:
-                normal_ports.append(port)
-        i = i + GAP_TIME # Counts the gap time between scans
-        scan = scan + 1
-        time.sleep(GAP_TIME) # 
+    try:
+        while i <= RUNNING_TIME:
+            # Once an hour, record the ports in use
+            print 'Starting scan #' + str(scan) + ': ' + str(i) + ' seconds in with ' + str(RUNNING_TIME - i) + ' seconds remaining'
+            ports = self_scan(False) # False means self-scan in verbose mode
+            print 'Open ports: '
+            if ports is not []:
+                print ports
+            else:
+                print 'None'
+            for port in ports:
+                if port not in normal_ports:
+                    normal_ports.append(port)
+            i = i + GAP_TIME # Counts the gap time between scans
+            scan = scan + 1
+            time.sleep(GAP_TIME)
+    except KeyboardInterrupt:
+        pass
     # Write the list of 'normal' ports to a file after the scan completes
     file = open('normal_ports.txt', 'w')
     for port in normal_ports:
@@ -96,24 +102,34 @@ def learning():
 
 # AUTONOMOUS MONITORING
 def auto_monitoring():
-    file = open('normal_ports.txt', 'r')
+    # Opens the file passed as an argument to -auto
+    file = open(args.auto, 'r')
     list = file.read()
-    print list
+    print '** Press CTRL-C at any time to stop the process'
+    print 'Ports found in list:'
     normal_ports = list.split(',')
-    while 1:
-        found_ports = self_scan()
-        print found_ports
-        for found in found_ports:
-            # found ports are integers
-            sketchy_port = False
-            for normal in normal_ports:
-                # normal ports are all strings
-                in_list = False
-                if str(found) == normal:
-                    in_list = True
-            if in_list == False:
-                sketchy_port = True
-                print 'ALERT: sketchy port detected'
+    for port in normal_ports: # Print out the ports in the file
+        if port != '':        # that we are going to label as normal
+            print '-' + port
+    try:
+        while True:
+            found_ports = self_scan(True) # Self-scan for open ports in quiet mode
+            sketchy_ports = [] # Integer list of ports that we have alerted users about
+            for found in found_ports:
+                # Found ports are integers
+                sketchy_port = False # First we assume the port isn't sketch
+                for normal in normal_ports:
+                    # Normal ports are all strings
+                    in_list = False  # Then we check if the port is in the list of normal ports
+                    if str(found) == normal:
+                        in_list = True
+                if in_list == False: # If this open port is not in the list, we tell the user it might be sketchy
+                    sketchy_port = True
+                    if found not in sketchy_ports: # Makes it so that we don't do too many alerts
+                        print 'ALERT: open port #' + str(found) + ' may be sketchy'
+                        sketchy_ports.append(found)
+    except KeyboardInterrupt:
+        pass
 
 # Main function that handles movement between the three modes
 args = parser.parse_args()
